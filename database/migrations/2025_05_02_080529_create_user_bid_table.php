a2397171b4fd36ec59850a0adf6e1df30d8af0f8
Denis Klevtsov <denis.klevtsovs@gmail.com>
2025-05-02 20:02:42 +0300

    migration fix

--- database/migrations/2025_05_02_080529_create_user_bid_table.php
+++ database/migrations/2025_05_02_080529_create_user_bid_table.php
@@ -25,6 +25,6 @@ return new class extends Migration
      */
     public function down(): void
     {
-        Schema::dropIfExists('users_bid_stories');
+        Schema::dropIfExists('user_bid');
     }
 };
