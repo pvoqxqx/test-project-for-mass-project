d744cd9e7fc09f0755f02269a80fa691660bcf0d
Denis Klevtsov <denis.klevtsovs@gmail.com>
2025-05-02 21:04:57 +0300

    small fix

--- app/Models/Bid.php
+++ app/Models/Bid.php
@@ -2,7 +2,7 @@
 
 namespace App\Models;
 
-use BidStatusChanged;
+use App\Notifications\BidStatusChanged;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Database\Eloquent\Relations\BelongsTo;
--- app/Notifications/BidStatusChanged.php
+++ app/Notifications/BidStatusChanged.php
@@ -1,4 +1,7 @@
 <?php
+
+namespace App\Notifications;
+
 use Illuminate\Notifications\Notification;
 use Illuminate\Notifications\Messages\MailMessage;
 
