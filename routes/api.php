fbe153f612504512b2aa15028927993836357590
Denis Klevtsov <denis.klevtsovs@gmail.com>
2025-05-02 14:16:45 +0300

    add swagger

--- routes/api.php
+++ routes/api.php
@@ -1,5 +1,6 @@
 <?php
 
+use App\Http\Controllers\Api\UserController;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
@@ -16,4 +17,12 @@ use Illuminate\Support\Facades\Route;
 
 Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
     return $request->user();
+});
+
+Route::post('/register', [UserController::class, 'register']);
+Route::get('/email/verify/{id}/{hash}', [UserController::class, 'verify'])->name('verification.verify');
+
+Route::middleware('auth:sanctum')->group(function () {
+    Route::put('/user', [UserController::class, 'update']);
+    Route::delete('/user/{id}', [UserController::class, 'destroy']);
 });
